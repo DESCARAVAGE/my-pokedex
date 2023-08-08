ad3c32b206895deb0a3c563972e11d1d4c7be616
daniel <daniel@daniel-Inspiron-7786>
2023-08-08 10:33:59 +0200

    condition d'affichage img

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,26 @@
+const pokemonList = [
+  {
+    name: "bulbizar",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+    imgSrc: null,
+  },
+];
+
 function PokemonCard() {
+  const pokemon = pokemonList[0];
+  console.log(pokemon.imgSrc);
   return (
     <figure>
-      <img
-        className="card"
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="Bulbizar est un pokemon de type herbe"
-      />
-      <figcaption> Bulbizar Lvl°13</figcaption>
+      {pokemon.imgSrc ? (
+        <img className="card" src={pokemon.imgSrc} alt="Image d'un pokemon" />
+      ) : (
+        <p>???</p>
+      )}
+      <figcaption> {pokemon.name} </figcaption>
     </figure>
   );
 }
