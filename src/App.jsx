3eebc44b3d895854f0a543af70b36165e40122c4
daniel <daniel@daniel-Inspiron-7786>
2023-08-08 09:26:58 +0200

    effet de hover sur card

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import MyTitle from "./components/MyTitle";
 import PokemonCard from "./components/PokemonCard";
+import "./components/PokemonCard.css";
 
 function App() {
 
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -2,6 +2,7 @@ function PokemonCard() {
   return (
     <figure>
       <img
+        className="card"
         src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
         alt="Bulbizar est un pokemon de type herbe"
       />
