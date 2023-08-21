25caa74fdcd4f5658d8d4dfb9d49e73e6a4c2070
daniel <daniel@daniel-Inspiron-7786>
2023-08-21 10:43:46 +0200

    intégration de state + ajout de pokemon + bouton de navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,68 @@
+import { useState } from "react";
 import "./App.css";
 import MyTitle from "./components/MyTitle";
 import PokemonCard from "./components/PokemonCard";
 import "./components/PokemonCard.css";
 
 function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+  const handleClickBefore = () => {
+    setPokemonIndex(pokemonIndex - 1);
+  };
+
+  const handleClickAfter = () => {
+    setPokemonIndex(pokemonIndex + 1);
+  };
+
+  /* const handleClickConsole = () => {
+    console.log(pokemonIndex);
+    console.log(pokemon);
+  }; */
+
   const pokemonList = [
     {
-      name: "bulbizar",
+      name: "bulbasaur",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
     {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
       name: "mew",
-      imgSrc: null,
     },
   ];
 
-  const pokemon = pokemonList[1];
+  const pokemon = pokemonList[pokemonIndex];
 
   return (
     <>
       <div>
         <MyTitle />
         <PokemonCard pokemon={pokemon} />
+        {pokemonIndex > 0 ? (
+          <button onClick={handleClickBefore}>Précédent</button>
+        ) : (
+          ""
+        )}
+        {pokemonIndex < pokemonList.length - 1 ? (
+          <button onClick={handleClickAfter}>Suivant</button>
+        ) : (
+          ""
+        )}
+        {/* <button onClick={handleClickConsole}>Console de pokemonIndex</button> */}
       </div>
     </>
   );
